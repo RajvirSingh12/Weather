f267634e2c4fe0d456fddf3a59214fae73ec04ca
RajvirSingh12 <54405659+RajvirSingh12@users.noreply.github.com>
2020-02-21 21:18:39 -0500

    Update weather.php

--- weather.php
+++ weather.php
@@ -4,7 +4,7 @@ sleep(2);
 $z_code = $_GET["z_code"];
 
 
-$url = "http://api.openweathermap.org/data/2.5/weather?zip=$z_code,us&APPID=bb84fb990a3633f854ed4a3fcf6dc252"; 
+$url = "http://api.openweathermap.org/data/2.5/weather?zip=$z_code,us&APPID=9ac78bf3aa1a9878ed1726ba8ab7d1f2"; 
 
 
 //Your API Key has to be obtained from openweathermap.org
